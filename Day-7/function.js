ef9fb72fce6ac3bf9a8b93dae37051c82d5584c9
Danish <danish58278@gmail.com>
2024-06-21 22:16:24 +0530

    commit -m "Day 7: Functions"

--- Day-7/function.js
+++ Day-7/function.js
@@ -1,13 +1,49 @@
-// 1.Declare a function fullName and it print out your full name
+// //1.Declare a function fullName and it print out your full name
 // function fullName(params) {
 //     let name = "Danish Ansari"
 //     console.log(name)
 // }
 // fullName()
 
-function fullName(fname,lname) {
-    fname ="";
-    lname="";
-    console.log(fullName)
+// //2.Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+// function fullName(fname,lname) {
+  
+//     console.log(fname+lname)
+// }
+// fullName("dan","ish")
+
+// //3.Declare a function addNumbers and it takes two two parameters and it returns sum.
+
+// function sum2n(a,b) {
+//     let sum = a+b;
+//     console.log(sum)
+// }
+
+// sum2n(4,9)
+
+// // 4.An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle.
+
+// function areaOfRectangle(length,breadth){
+//     let a = length * breadth;
+//     console.log(a)
+// }
+
+// areaOfRectangle(6,8)
+
+// // 5.A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
+
+// function perimeterOfRectangle(length,breadth) {
+//     let perimeter = 2*(length+breadth);
+//     console.log(perimeter)
+    
+// }
+
+// perimeterOfRectangle(4,5);
+
+// // 7. A volume of a rectangular prism is calculated as follows: volume = length x width x height. Write a function which calculates volumeOfRectPrism.
+
+function volume(l,b,h) {
+    let v = l*b*h;
+    console.log(v)    
 }
-fullName("dan","ans")
+volume(4,7,8)
