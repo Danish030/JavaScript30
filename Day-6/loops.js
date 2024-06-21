da890947b883eb99b7402b242908b9ce5100b272
Danish <danish58278@gmail.com>
2024-06-21 21:54:53 +0530

    new files

---
+++ Day-6/loops.js
@@ -0,0 +1,42 @@
+// 1. Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i) 
+// }
+
+// let i = 1;
+// while (i<=10) {
+//    console.log(i) 
+//    i++;
+// }
+
+// let j = 1;
+// do {
+//     console.log(j) 
+//     j++;
+// } while (j<=10)
+
+//  2.Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i) 
+// }
+
+// let i = 10;
+// while (i>=0) {
+//    console.log(i) 
+//    i--;
+// }
+
+// let j = 10;
+// do {
+//     console.log(j) 
+//     j--;
+// } while (j>=0)
+
+// 3.  Iterate 0 to n using for loop
+ let n = window.prompt("enter n")
+for (let i = 0; i < n; i++) {
+    console.log(i)
+}
+
---
+++ Day-7/function.js
@@ -0,0 +1,13 @@
+// 1.Declare a function fullName and it print out your full name
+// function fullName(params) {
+//     let name = "Danish Ansari"
+//     console.log(name)
+// }
+// fullName()
+
+function fullName(fname,lname) {
+    fname ="";
+    lname="";
+    console.log(fullName)
+}
+fullName("dan","ans")
